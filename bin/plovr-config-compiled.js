9412455886b3e42aefaf51cf29ceff0eda8bc247
Sebastian Poreba <sebastian.poreba@gmail.com>
2013-01-15 23:21:58 +0100

    Switch index.html to static js.

---
+++ bin/plovr-config-compiled.js
@@ -0,0 +1,21 @@
+{
+  "id": "rendering",
+  "paths": [
+    "../src",
+    "../lib"
+	],
+  "inputs": "../src/runner.js",
+  "mode": "ADVANCED",
+  "level": "SILENT",
+  "checks": {
+    "checkRegExp": "ERROR",
+    "checkTypes": "ERROR",
+    "checkVars": "ERROR",
+    "deprecated": "ERROR",
+    "fileoverviewTags": "ERROR",
+    "invalidCasts": "OFF",
+    "missingProperties": "ERROR",
+    "nonStandardJsDocs": "ERROR",
+    "undefinedVars": "ERROR"
+  }
+}
